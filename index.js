57ba1173f72091f49840726fcbaf91cdd65937c6
nancy sin <nsin@sbcglobal.net>
2018-04-25 03:15:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function scuberGreetingForFeet(distance){
   if (distance <= 400){
     return "This one is on me!"
-  } else if (distance > 2000 && distance< 2500){
+  } else if (distance >2000 && distance <2500){
     return "I will gladly take your thirty bucks."
   } else if (distance > 2500) {
     return "No can do."
@@ -13,7 +13,7 @@ function ternaryCheckCity(city){
   if (city === "NYC"){
     return "Ok, sounds good."
   } else if {
-    return "No go." 
+    return "No go."
   }
 
 
